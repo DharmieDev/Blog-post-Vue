310df9bd2c76833268d447eeddd394e215a969b1
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-05-20 18:17:08 +0100

    App created with dependencies

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HomeView from './views/HomeView.vue';
+import SidebarView from './views/SidebarView.vue';
+
+</script>
+
+<template>
+  <div class="grid grid-cols-[200px_1fr] bg-[#212121] justify-center min-h-lvh w-full">
+      <SidebarView />
+      <HomeView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/SidebarView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="bg-[#222831] min-h-lvh">
+		<h2>Blog Post App</h2>
+	</div>
+</template>
