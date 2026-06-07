544798a5dcf775e1edf39394978d1e3b882aaf34
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-06-07 10:48:53 +0100

    Updated more funtionalities in the app

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import "@/App.css"
 
 import App from './App.vue'
-import router from './router'
+import router from './router/routerPosts'
 
 const app = createApp(App)
 
