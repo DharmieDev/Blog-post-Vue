74c5e7df844e21d79269a9eba8ca00715a6e2a56
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-06-07 10:43:17 +0100

    Updated the sidebar file

---
+++ src/components/SidebarView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Menu } from '@boxicons/vue'
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
+
+<template>
+  <div class="h-14 md:flex md:flex-col bg-[#222831] md:min-h-lvh items-center md:items-start">
+    <h2 class="hidden md:block text-center mt-5! text-[23px] font-bold">Blog Post App</h2>
+    <nav>
+      <div class="flex gap-2">
+        <Menu class="flex md:hidden w-6 h-6" @click="open = !open" />
+        <RouterLink to="/" class="md:hidden" v-if="open">Home</RouterLink>
+      </div>
+      <RouterLink to="/" class="hidden md:flex">Home</RouterLink>
+    </nav>
+  </div>
+</template>
